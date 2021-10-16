b29988d482f4e5a46d8284be6c93a8d0a1261385
GeorgHenriquez <ghenriqu@espol.edu.ec>
2021-10-16 15:46:42 -0500

    Premiun Calculation

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,27 +1,72 @@
 package ec.edu.espol.workshops;
+import java.util.Scanner;
 
 public class CarInsurance {
-    private String estado_civil;
-    private String sexo;
-    private int edad;
-    private String permiso;
+	Scanner scanner = new Scanner(System.in);
+	
+    private String marital_status;
+    private String sex;
+    private int age;
+    private String permission;
     private int base_price = 500;
     
+    public void ProgramConsole() {
+        System.out.println("Enter customer data.");
+        System.out.print("Enter the customer's age:");
+        while(scanner.hasNextLine()){
+        	this.age = Integer.parseInt(scanner.nextLine());
+        }
+        System.out.print("Enter the sex of the customer (M/F):");
+        while(scanner.hasNextLine()){
+        	this.sex = scanner.nextLine();
+        }
+        System.out.print("Enter the marital status of the customer (married/not married):");
+        while(scanner.hasNextLine()){
+        	this.marital_status = scanner.nextLine();
+        }
+        System.out.print("Do you have a driving license?: (yes/no)");
+        while(scanner.hasNextLine()){
+        	this.permission = scanner.nextLine();
+        }
+        if (this.permission.equals("yes"))
+          this.premiunCalculation();
+        else
+          System.out.println("A driver's license is a must to buy car insurance.");
+      }
+    
+    private int premiunCalculation() {
+		if (this.age >= 80) {
+			System.out.println("We do not sell a car insurance to a person over 80 years old.");
+	    } else {
+	    	if(this.sex.equals("M") && this.marital_status.equals("not married") && this.age < 25)
+	    		this.base_price = this.base_price + 1500;
+	    	else if (this.sex.equals("F") || this.marital_status.equals("married"))
+	    		this.base_price = this.base_price - 200;
+	    	else if (this.age >= 45 && this.age < 65)
+	    		this.base_price = this.base_price - 100;
+	    	System.out.print("Its value is:");
+	    	System.out.println(this.base_price);
+	    	return this.base_price;
+	    }
+		System.out.print("An error has ocurred");
+		return -1;
+    }
+    
     /* Getter */
-	public String getEstado_civil() {
-		return this.estado_civil;
+	public String getmarital_status() {
+		return this.marital_status;
 	}
 	
-	public String getSexo() {
-		return this.sexo;
+	public String getSex() {
+		return this.sex;
 	}
 	
-	public int getEdad() {
-		return this.edad;
+	public int getage() {
+		return this.age;
 	}
 	
-	public String getPermiso() {
-		return this.permiso;
+	public String getpermission() {
+		return this.permission;
 	}
 	
 	public int getBase_price() {
@@ -29,20 +74,20 @@ public class CarInsurance {
 	}
 	
 	/* Setter */
-	public void setEstado_civil(String estado_civil) {
-		this.estado_civil = estado_civil;
+	public void setmarital_status(String marital_status) {
+		this.marital_status = marital_status;
 	}
 	
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
+	public void setSexo(String sex) {
+		this.sex = sex;
 	}
 	
-	public void setEdad(int edad) {
-		this.edad = edad;
+	public void setage(int age) {
+		this.age = age;
 	}
 	
-	public void setPermiso(String permiso) {
-		this.permiso = permiso;
+	public void setpermission(String permission) {
+		this.permission = permission;
 	}
 	
 	public void setBase_price(int new_price) {
