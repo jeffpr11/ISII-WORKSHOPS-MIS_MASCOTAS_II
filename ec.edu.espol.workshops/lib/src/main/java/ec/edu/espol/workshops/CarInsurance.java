f22e912335c9b75e4d2ed66ad3f53d4e9d9cf354
Eduardo Gonzalez <eduardogonzabay@gmail.com>
2021-10-16 14:13:18 -0500

    attr and class to setters and getters

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,5 +1,9 @@
 package ec.edu.espol.workshops;
 
 public class CarInsurance {
-
+    private String estado_civil;
+    private String sexo;
+    private int edad;
+    private String permiso;
+    private int base_price = 500;
 }
