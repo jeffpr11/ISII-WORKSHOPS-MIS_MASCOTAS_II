541c5b0b13057303b03a8a6e29b133c370606c5f
GeorgHenriquez <ghenriqu@espol.edu.ec>
2021-10-21 13:34:34 -0500

    checkstyle

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,96 +1,111 @@
 package ec.edu.espol.workshops;
+
+
 import java.util.Scanner;
 
+/**
+ * CarInsurance Class for program execution.
+ */
 public class CarInsurance {
-	Scanner scanner = new Scanner(System.in);
-	
-    private String marital_status;
-    private String sex;
-    private int age;
-    private String permission;
-    private int base_price = 500;
-    
-    public void ProgramConsole() {
-        System.out.println("Enter customer data.");
-        System.out.print("Enter the customer's age:");
-        while(scanner.hasNextLine()){
-        	this.age = Integer.parseInt(scanner.nextLine());
-        }
-        System.out.print("Enter the sex of the customer (M/F):");
-        while(scanner.hasNextLine()){
-        	this.sex = scanner.nextLine();
-        }
-        System.out.print("Enter the marital status of the customer (married/not married):");
-        while(scanner.hasNextLine()){
-        	this.marital_status = scanner.nextLine();
-        }
-        System.out.print("Do you have a driving license?: (yes/no)");
-        while(scanner.hasNextLine()){
-        	this.permission = scanner.nextLine();
-        }
-        if (this.permission.equals("yes"))
-          this.premiunCalculation();
-        else
-          System.out.println("A driver's license is a must to buy car insurance.");
-      }
-    
-    private int premiunCalculation() {
-		if (this.age >= 80) {
-			System.out.println("We do not sell a car insurance to a person over 80 years old.");
-	    } else {
-	    	if(this.sex.equals("M") && this.marital_status.equals("not married") && this.age < 25)
-	    		this.base_price = this.base_price + 1500;
-	    	else if (this.sex.equals("F") || this.marital_status.equals("married"))
-	    		this.base_price = this.base_price - 200;
-	    	else if (this.age >= 45 && this.age < 65)
-	    		this.base_price = this.base_price - 100;
-	    	System.out.print("Its value is:");
-	    	System.out.println(this.base_price);
-	    	return this.base_price;
-	    }
-		System.out.print("An error has ocurred");
-		return -1;
+  Scanner scanner = new Scanner(System.in);
+
+  private String maritalStatus;
+  private String sex;
+  private int age;
+  private String permission;
+  private int basePrice = 500;
+
+  /**
+   * programConsole method.
+   * It allows inputs where customer set his data to calculate the value.
+   */
+  public void programConsole() {
+    System.out.println("Enter customer data.");
+    System.out.print("Enter the customer's age:");
+    while (scanner.hasNextLine()) {
+      this.age = Integer.parseInt(scanner.nextLine());
     }
-    
-    /* Getter */
-	public String getmarital_status() {
-		return this.marital_status;
-	}
-	
-	public String getSex() {
-		return this.sex;
-	}
-	
-	public int getage() {
-		return this.age;
-	}
-	
-	public String getpermission() {
-		return this.permission;
-	}
-	
-	public int getBase_price() {
-		return this.base_price;
-	}
-	
-	/* Setter */
-	public void setmarital_status(String marital_status) {
-		this.marital_status = marital_status;
-	}
-	
-	public void setSexo(String sex) {
-		this.sex = sex;
-	}
-	
-	public void setage(int age) {
-		this.age = age;
-	}
-	
-	public void setpermission(String permission) {
-		this.permission = permission;
-	}
-	
-	public void setBase_price(int new_price) {
-		this.base_price = this.base_price + new_price;
-	}
+    System.out.print("Enter the sex of the customer (M/F):");
+    while (scanner.hasNextLine()) {
+      this.sex = scanner.nextLine();
+    }
+    System.out.print("Enter the marital status of the customer (married/not married):");
+    while (scanner.hasNextLine()) {
+      this.maritalStatus = scanner.nextLine();
+    }
+    System.out.print("Do you have a driving license?: (yes/no)");
+    while (scanner.hasNextLine()) {
+      this.permission = scanner.nextLine();
+    }
+    if (this.permission.equals("yes")) {
+      this.premiunCalculation();
+    } else {
+      System.out.println("A driver's license is a must to buy car insurance.");
+    }
+  }
+
+  /**
+   * premiunCalculation method.
+   * According to age, sex and marital status it calculates the value.
+   */
+  private int premiunCalculation() {
+    if (this.age >= 80) {
+      System.out.println("We do not sell a car insurance to a person over 80 years old.");
+    } else {
+      if (this.sex.equals("M") && this.maritalStatus.equals("not married") && this.age < 25) {
+        this.basePrice = this.basePrice + 1500;
+      } else if (this.sex.equals("F") || this.maritalStatus.equals("married")) {
+        this.basePrice = this.basePrice - 200;
+      } else if (this.age >= 45 && this.age < 65) {
+        this.basePrice = this.basePrice - 100;
+      }
+      System.out.print("Its value is:");
+      System.out.println(this.basePrice);
+      return this.basePrice;
+    }
+    System.out.print("An error has ocurred");
+    return -1;
+  }
+
+  /* Getter */
+  public String getMaritalStatus() {
+    return this.maritalStatus;
+  }
+
+  public String getSex() {
+    return this.sex;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+
+  public String getPermission() {
+    return this.permission;
+  }
+
+  public int getBasePrice() {
+    return this.basePrice;
+  }
+
+  /* Setter */
+  public void setMaritalStatus(String maritalStatus) {
+    this.maritalStatus = maritalStatus;
+  }
+
+  public void setSex(String sex) {
+    this.sex = sex;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public void setPermission(String permission) {
+    this.permission = permission;
+  }
+
+  public void setBasePrice(int newPrice) {
+    this.basePrice = this.basePrice + newPrice;
+  }
 }
