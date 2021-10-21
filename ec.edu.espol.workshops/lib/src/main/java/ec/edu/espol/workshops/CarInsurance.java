04d50ba4c89304f1d63b07f007bd56a7215291dd
Eduardo Gonzalez <eduardogonzabay@gmail.com>
2021-10-21 13:21:35 -0500

    fixes bug dice PMD

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,54 +1,56 @@
 package ec.edu.espol.workshops;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class CarInsurance {
 	Scanner scanner = new Scanner(System.in);
+	PrintStream out = System.out;
 	
     private String marital_status;
     private String sex;
     private int age;
     private String permission;
     private int base_price = 500;
-    
+
     public void ProgramConsole() {
-        System.out.println("Enter customer data.");
-        System.out.print("Enter the customer's age:");
+        out.println("Enter customer data.");
+        out.print("Enter the customer's age:");
         while(scanner.hasNextLine()){
         	this.age = Integer.parseInt(scanner.nextLine());
         }
-        System.out.print("Enter the sex of the customer (M/F):");
+        out.print("Enter the sex of the customer (M/F):");
         while(scanner.hasNextLine()){
         	this.sex = scanner.nextLine();
         }
-        System.out.print("Enter the marital status of the customer (married/not married):");
+        out.print("Enter the marital status of the customer (married/not married):");
         while(scanner.hasNextLine()){
         	this.marital_status = scanner.nextLine();
         }
-        System.out.print("Do you have a driving license?: (yes/no)");
+        out.print("Do you have a driving license?: (yes/no)");
         while(scanner.hasNextLine()){
         	this.permission = scanner.nextLine();
         }
-        if (this.permission.equals("yes"))
+        if (("yes").equals(this.permission))
           this.premiunCalculation();
         else
-          System.out.println("A driver's license is a must to buy car insurance.");
+          out.println("A driver's license is a must to buy car insurance.");
       }
     
     private int premiunCalculation() {
 		if (this.age >= 80) {
-			System.out.println("We do not sell a car insurance to a person over 80 years old.");
+			out.println("We do not sell a car insurance to a person over 80 years old.");
 	    } else {
-	    	if(this.sex.equals("M") && this.marital_status.equals("not married") && this.age < 25)
+	    	if(("M").equals(this.sex) && ("not married").equals(this.marital_status) && this.age < 25)
 	    		this.base_price = this.base_price + 1500;
-	    	else if (this.sex.equals("F") || this.marital_status.equals("married"))
+	    	else if (("F").equals(this.sex) || ("married").equals(this.marital_status))
 	    		this.base_price = this.base_price - 200;
 	    	else if (this.age >= 45 && this.age < 65)
 	    		this.base_price = this.base_price - 100;
-	    	System.out.print("Its value is:");
-	    	System.out.println(this.base_price);
+	    	out.print("Its value is:");
+	    	out.println(this.base_price);
 	    	return this.base_price;
 	    }
-		System.out.print("An error has ocurred");
+		out.print("An error has ocurred");
 		return -1;
     }
     
